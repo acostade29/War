5eefd832dbeaaea9115c7b646f943df18fe97330
Michael Lackey <michael.g.lackey@gmail.com>
2020-02-08 13:46:34 -0600

    create deal and play logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /*----- constants -----*/
 const cards = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"];
-let p1Cards = [];
-let p2Cards = [];
-let p1Flipped;
-let p2Flipped;
 
 
 /*----- app's state (variables) -----*/
+let p1Cards = [];
+let p2Cards = [];
+let p1Flipped = [];
+let p2Flipped = [];
 
 
 /*----- cached element references -----*/
@@ -15,51 +15,68 @@ const p2deck = document.getElementById('p2deck');
 const p1flip = document.getElementById('p1flip');
 const p2flip = document.getElementById('p2flip');
 const dealButton = document.getElementById('deal-button');
+const playButton = document.getElementById('play-button');
 
 
 /*----- event listeners -----*/
 dealButton.addEventListener('click', dealCards);
+playButton.addEventListener('click', playCards);
 
 
 /*----- functions -----*/
-init();
+function shuffle() {
+    let idx = cards.length, temp, rndIdx;
+    while (0 !== idx) {
+      rndIdx = Math.floor(Math.random() * idx);
+      idx -= 1;
+      temp = cards[idx];
+      cards[idx] = cards[rndIdx];
+      cards[rndIdx] = temp;
+    }
+};
 
 function dealCards() {
+    shuffle();
+    if (cards.length) {
+        for (let i = 0; i < 26; i++) {
+            p1Dealt = cards.splice(0, 1);
+            p1Cards.push(p1Dealt[0]);
+        }
+        for (let i = 0; i < 26; i++) {
+            p2Dealt = cards.splice(0, 1);
+            p2Cards.push(p2Dealt[0]);
+        }
+    }
+    p1deck.classList.add('back-blue', 'shadow-medium');
+    p2deck.classList.add('back-blue', 'shadow-medium');
+    render();
+};
+
+function playCards() {
     if (p1Cards.length) {
-        let rndIdx = Math.floor(Math.random() * p1Cards.length);
         p1RemovedCard = p1Flipped;
-        p1Flipped = cards.splice(rndIdx, 1);
+        p1Flipped = p1Cards.splice(0, 1);
         p1Cards.push(p1Flipped[0]);
+        p1flip.classList.replace('outline', p1Flipped);
+    }
+    if (p2Cards.length) {
         p2RemovedCard = p2Flipped;
-        p2Flipped = cards.splice(rndIdx, 1);
+        p2Flipped = p2Cards.splice(0, 1);
         p2Cards.push(p2Flipped[0]);
+        p2flip.classList.replace('outline', p2Flipped);
     }
     render();
 };
 
 function render() {
-    // Card flip logic
-    if (p1Flipped.length === 1) {
-        p1flip.classList.replace('outline', p1Flipped);
-    } else {
-        p1flip.classList.replace(p1RemovedCard, p1Flipped);
-    }
-    if (p2Flipped.length === 1) {
-        p2flip.classList.replace('outline', p2Flipped);
-    } else {
-        p2flip.classList.replace(p2RemovedCard, p2Flipped);
-    }
     // Player 1 card shading
     if (p1Cards.length === 0) {
         p1deck.classList.replace('back-blue', 'outline');
     }
-    if (p1Cards.length > 0) {
-        p1deck.classList.add('shadow-none');
-    }
     if (p1Cards.length > 13) {
-        p1deck.classList.replace('shadow-none', 'shadow-light');
+        p1deck.classList.replace('outline', 'shadow-light');
     }
-    if (p1Cards.length > 26) {
+    if (p1Cards.length >= 26) {
         p1deck.classList.replace('shadow-light', 'shadow-medium');
     }
     if (p1Cards.length > 39) {
@@ -72,13 +89,10 @@ function render() {
     if (p2Cards.length === 0) {
         p2deck.classList.replace('back-blue', 'outline');
     }
-    if (p2Cards.length > 0) {
-        p2deck.classList.add('shadow-none');
-    }
     if (p2Cards.length > 13) {
-        p2deck.classList.replace('shadow-none', 'shadow-light');
+        p2deck.classList.replace('outline', 'shadow-light');
     }
-    if (p2Cards.length > 26) {
+    if (p2Cards.length >= 26) {
         p2deck.classList.replace('shadow-light', 'shadow-medium');
     }
     if (p2Cards.length > 39) {
@@ -86,17 +100,5 @@ function render() {
     }
     if (p2Cards.length > 51) {
         p2deck.classList.replace('shadow-medium', 'shadow-full');
-    }
-};
-
-function init() {
-    for (let i = 0; i < cards.length; i++) {
-    if (cards.length) {
-    let rndIdx = Math.floor(Math.random() * cards.length);
-    p1Dealt = cards.splice(rndIdx, 1);
-    p1Cards.push(p1Dealt[0]);
-    p2Dealt = cards.splice(rndIdx, 1);
-    p2Cards.push(p2Dealt[0]);
-    }
     }
 };
